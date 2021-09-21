e12c2420a280e9728f8b9e4a8987cde8416a5d77
ezzee01 <ezec_01@hotmail.com>
2021-09-20 23:51:19 -0300

    desafio 1

---
+++ clase1 carranza ezequiel/desafio.js
@@ -0,0 +1,12 @@
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese apellido");
+let edad = prompt("Ingrese edad");
+
+alert( nombre + " " + apellido + " " + edad);
+
+alert("CALCULADORA");
+
+let numeroUno = parseInt(prompt("Ingrese numero"));
+let numeroDos = parseInt(prompt("Ingrese numero"));
+
+alert(numeroUno + numeroDos);
